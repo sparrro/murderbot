d0c1834fcd9ff9fd4bab892e7477b73a1b202ad6
Rolf Sparv <hrothi.wulfaz@gmail.com>
2026-07-15 13:54:12 +0200

    quarantine user to be tested

--- src/utils/weapons.ts
+++ src/utils/weapons.ts
@@ -4,13 +4,23 @@ import { findByUsername, findFather, findMods } from "./findMemberFunctions";
 export const quarantineUserAndAlertMods = async (user: GuildMember, message: Message) => {
     const mods = await findMods(user.guild);
     const father = await findFather(user.guild);
-    for (const mod of mods) {
-        try {
-            await mod.send(`I have put ${user.displayName} because I believe their message ${message.content} was spam and would like a human moderator to investigate further`)
-        } catch {
-            const errorMsg = `Failed to dm mod ${mod.displayName} regarding ${user.displayName} being quarantined`
-            console.log(errorMsg)
-            await father?.send(errorMsg);
+    let timedOut: number | null | undefined
+    try {
+        timedOut = (await user.timeout(1000 * 60 * 60 * 24)).communicationDisabledUntilTimestamp;
+    } catch {
+        const errorMsg = `Failed to quarantine ${user.displayName}`;
+        console.log(errorMsg);
+        await father?.send(errorMsg);
+    }
+    if (timedOut) {
+        for (const mod of mods) {
+            try {
+                await mod.send(`I have put ${user.displayName} in quarantine because I believe their message ${message.content} was spam and would like a human moderator to make a final determination`);
+            } catch {
+                const errorMsg = `Failed to dm mod ${mod.displayName} regarding ${user.displayName} being quarantined`;
+                console.log(errorMsg);
+                await father?.send(errorMsg);
+            }
         }
     }
 };
